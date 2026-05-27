80bdd7587af5de7a0ae8bd3a4792b3c2b2e9727f
Raj Dave <rajdave03rh@gmail.com>
2026-05-27 13:20:51 +0530

    small fix

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 console.log("Hey There, finally my package is published ......");
 
 export const addValues = (...args: [number]) => {
-  args.reduce((acc, curr) => {
+  return args.reduce((acc, curr) => {
     return curr + acc;
   }, 0);
 };
